991d4ae61cda899513fba2eb635230c58fd1d79e
Heorhii Pluhovyi <heorhiipluhovui@gmail.com>
2023-02-04 06:14:17 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ServerElement } from './types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ComponentsAndDatabindingDeepDive';
+  serverElements: ServerElement[] = [];
+
+  addServer(serverData: ServerElement) {
+    this.serverElements.push(serverData);
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'chenged'
+  }
+
+  onDestroyFirstEl() {
+    this.serverElements.splice(0, 1)
+  }
 }
